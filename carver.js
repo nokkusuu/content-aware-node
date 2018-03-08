4cf36eeb4ff726bfce70ab6ad9c7eee77aab6ec5
ayunami2000 <spwilliamsiam@gmail.com>
2018-03-08 07:17:52 -0500

    Update carver.js

--- carver.js
+++ carver.js
@@ -68,7 +68,7 @@
 		window.memtext.save();
 		window.memtext.translate(0,window.memvas.height);
 		window.memtext.rotate(Math.PI/-2);
-		window.memtext.scale(1.05,1.05);
+		window.memtext.scale(200-x,200-y);
 		window.memtext.drawImage(imgd,0,0);
 		window.memtext.restore();
 		window.c.canvas.style.display=window.d.canvas.style.display="none";
