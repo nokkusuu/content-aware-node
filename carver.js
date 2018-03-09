0fedbdbb6dd9cf7977dac5a8c8039382b97c5964
ayunami2000 <spwilliamsiam@gmail.com>
2018-03-09 08:04:29 -0500

    Update carver.js

--- carver.js
+++ carver.js
@@ -14,7 +14,7 @@
 		img.onload=function(){img.onload=function(){};window.gif.addFrame(img,{delay:50});}
 		img.src=url;
 		for(var i=0;i<20;i++){
-			doEverything(btn,95,95,window.url,function(url){window.url=url;window.wait=0;});
+			doEverything(btn,1,window.url,function(url){window.url=url;window.wait=0;});
 			while(window.wait){await new Promise(sleep=>setTimeout(sleep,0));}
 			window.gif.addFrame(window.memvas,{delay:50});
 			window.wait=1;
@@ -23,21 +23,21 @@
 		window.gif.render();
 		btn.removeAttribute("disabled");
 	}
-	function doEverything(btn,x,y,url,cb){
+	function doEverything(btn,gif,url,cb){
 		btn.setAttribute('disabled','disabled');
 		var img=document.getElementById('i');
 		document.getElementById("res").style.display="none";
 		img.src="";
-		img.onload=function(){img.onload=function(){};doCarve(img.src,x/100,y/100,cb,[btn]);}
+		img.onload=function(){img.onload=function(){};doCarve(img.src,gif,cb,[btn]);}
 		img.src=url;
 	}
-	async function doCarve(url, x, y, callback, callbackvars){try{
+	async function doCarve(url, gif, callback, callbackvars){try{
 		window.memvas=document.createElement('canvas');
 		window.memtext=window.memvas.getContext('2d');
 		window.c=new Carver("c",url);
 		while(typeof window.c.img==="undefined"){await new Promise(sleep=>setTimeout(sleep,0));}
 		window.c.canvas.style.display="";
-		while(window.c.w>window.c.canvas.width*x){
+		while(window.c.w>window.c.canvas.width*(gif?0.5:0.95)){
 			window.c.shrink();
 			await new Promise(sleep=>setTimeout(sleep,document.querySelector('input[type=checkbox]').checked?0:100));
 		}
@@ -55,7 +55,7 @@
 		window.d=new Carver("d",window.memvas.toDataURL("image/png"));
 		while(typeof window.d.img==="undefined"){await new Promise(sleep=>setTimeout(sleep,0));}
 		window.d.canvas.style.display="";
-		while(window.d.w>window.d.canvas.width*y){
+		while(window.d.w>window.d.canvas.width*gif?0.5:0.95){
 			window.d.shrink();
 			await new Promise(sleep=>setTimeout(sleep,document.querySelector('input[type=checkbox]').checked?0:100));
 		}
@@ -66,7 +66,7 @@
 		window.memvas.height=window.d.canvas.width;
 		window.memtext.save();
 		window.memtext.translate(0,window.memvas.height);
-		window.memtext.scale((-2.11*x)+3.06,(-2.11*y)+3.06);//2-x,2-y
+		window.memtext.scale(gif?2:1.05,gif?2:1.05);
 		window.memtext.rotate(Math.PI/-2);
 		window.memtext.drawImage(imgd,0,0);
 		window.memtext.restore();
