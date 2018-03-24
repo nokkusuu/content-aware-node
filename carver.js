b9dc4a88c39124c082558277783422eb471bd44c
ayunami2000 <spwilliamsiam@gmail.com>
2018-03-24 13:27:15 -0400

    Update carver.js

--- carver.js
+++ carver.js
@@ -52,7 +52,7 @@
 		window.d=new Carver("d",window.memvas.toDataURL("image/png"));
 		while(typeof window.d.img==="undefined"){await new Promise(sleep=>setTimeout(sleep,0));}
 		window.d.canvas.style.display="";
-		document.querySelector("progress").max=window.d.canvas.width;
+		document.querySelector("progress").max=window.d.canvas.width/20;
 		while(window.d.w>window.d.canvas.width*(gif?0.95:0.5)){
 			window.d.shrink();
 			document.querySelector("progress").value=(1.5*window.d.canvas.width)-window.d.w;
